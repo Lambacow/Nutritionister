6c4e3e8b80754ca5526b0bf83579cb90e6d6ec43
Joey Lam <joeulam345@gmail.com>
2024-07-12 19:59:13 -0400

    Project initialization

---
+++ app/loginPages/signUp.tsx
@@ -0,0 +1,23 @@
+import { Image, StyleSheet, Platform, View } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <div>
+      <title>Login</title>
+    </div>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  
+});
