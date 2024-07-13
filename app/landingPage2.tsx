e6176f9303ba439953cee5111d2ae1e3866ad997
Joey Lam <joeulam345@gmail.com>
2024-07-12 23:12:51 -0400

    -Updated readme - Added page 2 - Added page 3 - Added login page

--- app/landingPage2.tsx
+++ app/landingPage2.tsx
@@ -1,6 +1,6 @@
 import {StyleSheet, Text, View} from 'react-native';
 
-export default function Index() {
+export default function landingPage2() {
   return (
     <View style={styles.background}>
       <View style={styles.titleCard}>
