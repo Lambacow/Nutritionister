2d566c045b27b81c547bb46cff7ac51c1d5d7772
Joey Lam <joeulam345@gmail.com>
2024-07-14 21:10:14 -0400

    started firebase signup function

--- app/(loginpages)/signUp.tsx
+++ app/(loginpages)/signUp.tsx
@@ -1,23 +1,64 @@
-import { Image, StyleSheet, Platform, View } from 'react-native';
+import {Image, Pressable, Button , StyleSheet, Text, View,} from "react-native";
+import React from 'react';
 
+import { TextInput } from "react-native-gesture-handler";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
-export default function HomeScreen() {
+export default function Index() {
+  const [text, username] = React.useState('');
+  const [value, password] = React.useState('');
+
   return (
-    <div>
-      <title>Login</title>
-    </div>
+    <View style={styles.background}>
+      <Text>Email</Text>
+      <TextInput
+        onChangeText={username}
+        value={text}
+      />
+
+      <Text>Password</Text>
+      <TextInput
+        onChangeText={password}
+        value={value}
+      />
+      
+      <Button onPress={signup(username,password)} title="Submit"></Button>
+    </View>    
+    
   );
 }
 
+function signup(email, password){
+  const auth = getAuth();
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+}
+
+
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  background:{
+    backgroundColor: "#758471",
+    height:"100%"
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  button:{
+    marginBottom:"3vh",
+        width:251,
+        height:44,
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius:20,
+        backgroundColor: '#80ED99',
   },
-  
+  buttonText:{
+    fontSize:24,
+    },
 });
